b139007a4578371bccbf7430e991808e154096a5
Ted Naleid <contact@naleid.com>
2026-04-03 16:52:38 -0500

    refactor after move to dropbox

--- zsh.d/starling.sh
+++ zsh.d/starling.sh
@@ -1,23 +1,27 @@
 # computer specific zshrc settings
 
-export WORKSPACE_DIR="${HOME}/Documents/archives/workspace"
-alias cdw='cd ${WORKSPACE_DIR}'
+export DROPBOX_DIR="${HOME}/Library/CloudStorage/Dropbox"
 
-export DOTFILES_DIR="${WORKSPACE_DIR}/dotfiles"
+export CODE_DIR="${DROPBOX_DIR}/code"
+alias cdc='cd ${CODE_DIR}'
+
+export DOTFILES_DIR="${CODE_DIR}/dotfiles"
 alias cdd='cd ${DOTFILES_DIR}'
 alias codedd='code ${DOTFILES_DIR}'
 
+
+export DOCS_DIR="${DROPBOX_DIR}/docs"
 # obsidian markdown notes
-export MARKDOWN_NOTES_DIR="$HOME/Documents/archives/docs/notes"
+export MARKDOWN_NOTES_DIR="${DOCS_DIR}/notes"
 alias cdn="cd $MARKDOWN_NOTES_DIR"
 alias on='open "obsidian://open?vault=notes"'
 
-export PERSONAL_MARKDOWN_NOTES_DIR="$HOME/Documents/archives/docs/personal_markdown_notes"
+export PERSONAL_MARKDOWN_NOTES_DIR="${DOCS_DIR}/personal_markdown_notes"
 alias cdpn="cd $PERSONAL_MARKDOWN_NOTES_DIR"
 alias opn='open "obsidian://open?vault=personal_markdown_notes"'
 
-export CLIPPINGS_NOTES_DIR="$HOME/Documents/archives/docs/clippings"
-alias cdc="cd $CLIPPINGS_NOTES_DIR"
+export CLIPPINGS_NOTES_DIR="${DOCS_DIR}/clippings"
+alias cdcl="cd $CLIPPINGS_NOTES_DIR"
 alias oc='open "obsidian://open?vault=clippings"'
 
 function rgn() {
@@ -29,13 +33,15 @@ function rgc() {
     rg -i $@ "${CLIPPINGS_NOTES_DIR}"
 }
 
-alias cdnci="cd ${WORKSPACE_DIR}/naleid-consulting/invoices"
+alias cdnci="cd ${CODE_DIR}/naleid-consulting/invoices"
 
 # golang paths
-export GOPATH=$WORKSPACE_DIR/go
-
-if [[ -d "$WORKSPACE_DIR/go/bin" ]]; then
-  export PATH="$WORKSPACE_DIR/go/bin:$PATH"
+export GOPATH="${CODE_DIR}/go"
+if [[ -d "${GOPATH}" ]]; then
+  mkdir -p "${GOPATH}"
 fi
 
-# alias claude="/Users/tednaleid/.claude/local/claude"
+
+if [[ -d "${GOPATH}/bin" ]]; then
+  export PATH="${GOPATH}/bin:$PATH"
+fi
